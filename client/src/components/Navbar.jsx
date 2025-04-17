95a22059e17661e664d59aa76fc19e997c30e414
anudeepmuppalla1729 <anudeepmuppalla@gmail.com>
2025-04-17 15:54:02 +0530

    j

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,6 +7,9 @@ const Navbar = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
   const handleSearch = (query) => {
+    if (query != '') {
+    dispatch(searchNews(query));
+    }
     dispatch(searchNews(query));
   };
 
